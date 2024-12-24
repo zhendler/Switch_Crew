657ad1b80afc86fe04b7656002137fdd6c826620
krutsvitya <krutsvitya@gmail.com>
2024-12-24 10:55:30 +0100

    added tag's repos and routs

---
+++ src/tags/repos.py
@@ -0,0 +1,55 @@
+from http.client import HTTPException
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from ..models.models import Tag
+from sqlalchemy.future import select
+
+class TagRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def get_tag_by_name(self, tag_name: str):
+        tag = select(Tag).filter(Tag.name == tag_name)
+        result = await self.db.execute(tag)
+        return result.scalars().first()
+
+    async def create_tag(self, name: str):
+        new_tag = Tag(name=name)
+        self.db.add(new_tag)
+        await self.db.commit()
+        await self.db.refresh(new_tag)
+        return new_tag
+
+    async def get_all_tags(self):
+        tags = await self.db.execute(select(Tag))
+        return tags.scalars().all()
+
+    async def delete_tag_by_name(self, tag_name: str):
+        tag = select(Tag).filter(Tag.name == tag_name)
+        result = await self.db.execute(tag)
+        tag = result.scalars().first()
+        if tag:
+            await self.db.delete(tag)
+            await self.db.commit()
+            return "Successfully deleted!"
+        else:
+            return "Tag not found!"
+
+    async def update_tag_name(self, tag_name: str, tag_new_name: str):
+        tag = select(Tag).filter(Tag.name == tag_name)
+        result = await self.db.execute(tag)
+        tag = result.scalars().first()
+
+        if tag:
+            tag.name = tag_new_name
+            await self.db.commit()
+            await self.db.refresh(tag)
+            return tag
+        else:
+            return HTTPException(status_code=404, detail="Tag not found!")
+
+    async def get_photos_by_tag(self, tag_name: str):
+        tag = select(Tag).filter(Tag.name == tag_name)
+        result = await self.db.execute(tag)
+
+        return result.photos
