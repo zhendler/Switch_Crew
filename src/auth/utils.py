e4398d5a235dee181847a2b30206278f273989f2
krutsvitya <krutsvitya@gmail.com>
2024-12-26 19:41:46 +0100

    added dependencies + updated front

---
+++ src/auth/utils.py
@@ -0,0 +1,101 @@
+from fastapi import Depends, status, HTTPException
+from jose import jwt, JWTError
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from datetime import datetime, timedelta, timezone
+
+from src.auth.schemas import TokenData, RoleEnum
+from src.auth.repos import UserRepository
+from src.models.models import User
+from config.general import settings
+from config.db import get_db
+
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+REFRESH_TOKEN_EXPIRE_DAYS = settings.refresh_token_expire_days
+VERIFICATION_TOKEN_EXPIRE_HOURS = settings.verification_token_expire_hours
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+
+def create_verification_token(email: str) -> str:
+    expire = datetime.now(timezone.utc) + timedelta(
+        hours=VERIFICATION_TOKEN_EXPIRE_HOURS
+    )
+    to_encode = {"exp": expire, "sub": email}
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def decode_verification_token(token: str) -> str | None:
+    try:
+        payload = jwt.decode(token, settings.secret_key, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            return None
+        return email
+    except JWTError:
+        return None
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encode_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=ALGORITHM)
+    return encode_jwt
+
+
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire})
+    encode_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=ALGORITHM)
+    return encode_jwt
+
+
+def decode_access_token(token: str) -> TokenData | None:
+    try:
+        payload = jwt.decode(token, settings.secret_key, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            return None
+        return TokenData(username=username)
+    except JWTError:
+        return None
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme),
+                           db: AsyncSession = Depends(get_db)) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = decode_access_token(token)
+    if token_data is None:
+        raise credentials_exception
+    user_repo = UserRepository(db)
+    user = await user_repo.get_user_by_username(token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+class RoleChecker:
+
+    def __init__(self, allowed_roles: list[RoleEnum]):
+        self.allowed_roles = allowed_roles
+
+    async def __call__(
+            self, token: str = Depends(oauth2_scheme),
+            db: AsyncSession = Depends(get_db)
+    ) -> User:
+        user = await get_current_user(token, db)
+        is_admin_or_moderator = user.role.name in [RoleEnum.ADMIN.value, RoleEnum.MODERATOR.value]
+        if user.role.name not in [role.value for role in self.allowed_roles]:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You do not have permission to perform this action"
+            )
+        return user, is_admin_or_moderator
