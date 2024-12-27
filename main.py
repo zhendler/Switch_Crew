6bba49d3bddf3514d71b013754dbbc8eb253294c
krutsvitya <krutsvitya@gmail.com>
2024-12-27 19:09:44 +0100

    fixed main

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from src.tags.routers import tag_router, get_user
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from src.auth.routers import router as auth_router
 from fastapi.security import OAuth2PasswordBearer
-from src.photos.routers import photo_router
 app = FastAPI()
 
 app.include_router(tag_router, prefix='/tags', tags=['tags'])
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
-app.include_router(photo_router, prefix="/photos", tags=["photos"])
 
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -23,8 +21,7 @@ async def ping():
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root( request: Request):
-    user = get_user(request)
-    return templates.TemplateResponse("index.html", {"request": request, 'user': user})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/login", response_class=HTMLResponse)
 async def login(request: Request):
